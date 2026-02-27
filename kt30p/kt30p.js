5bbd35e484d117381c194a143503d8102de244d1
tdat207 <datdr2007@gmail.com>
2026-02-27 07:48:17 +0700

    Add files via upload

---
+++ kt30p/kt30p.js
@@ -0,0 +1,14 @@
+let students = [
+    { id: 1, name: "Nguyên A", score: 8.5, gender: "Nam" },
+    { id: 2, name: "Trân Thị B", score: 4.2, gender: "Nữ" },
+    { id: 3, name: "Lê Văn C", score: 9.0, gender: "Nam" },
+    { id: 4, name: "Phạm Thị D", score: 5.5, gender: "Nữ" },
+    { id: 5, name: "Hoàng Văn E", score: 3.8, gender: "Nam" }
+];
+
+const excellentStudent = (students);
+console.log("Danh sách học sinh viên xuất sắc: ",excellentStudent);
+
+const studentLabels = (students) => {
+    const 
+};
